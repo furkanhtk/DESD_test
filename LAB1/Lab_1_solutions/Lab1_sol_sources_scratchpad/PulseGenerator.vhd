f39bda87e35a9f21795c6de002504544b85d8267
Furkanhtk <furkanhtk@gmail.com>
2022-03-27 17:28:06 +0200

    -- RANGE_COUNT_FINE to RANGE_COUNT_DTO -- RANGE_COUNT_COARSE to RANGE_COUNT_SPEED -- count_fine to count_dto -- count_coarse to count_speed -- select_speedto speed_input --select_speed_reg to speed_input_reg1 and added speed_input_reg2

--- LAB1/Lab_1_solutions/Lab1_sol_sources_scratchpad/PulseGenerator.vhd
+++ LAB1/Lab_1_solutions/Lab1_sol_sources_scratchpad/PulseGenerator.vhd
@@ -22,7 +22,7 @@ entity PulseGenerator is
 		----------------------------
 
 		---------- Speed -----------
-		SWs		    :	IN	STD_LOGIC_VECTOR(NUM_OF_SWS-1 downto 0);	-- Switches avaiable on Besys3
+		switches	:	IN	STD_LOGIC_VECTOR(NUM_OF_SWS-1 downto 0);	-- Switches avaiable on Besys3
 		count_pulse	:	OUT	STD_LOGIC									-- signal which pulses when kit has to shift
 		----------------------------
 
@@ -36,30 +36,45 @@ architecture Behavioral of PulseGenerator is
 	------------------ CONSTANT DECLARATION -------------------------
 
     ---------- TIMER -----------
-	constant RANGE_COUNT_FINE		: positive		:= ((MIN_KITT_CAR_STEP_MS*10)/CLK_PERIOD_NS);  		
-	-- Simulation
+	constant RANGE_COUNT_DTO		: positive		:= ((MIN_KITT_CAR_STEP_MS*1000000)/CLK_PERIOD_NS);
+
+	-- RANGE_COUNT_FINE to RANGE_COUNT_DTO
+	-- Implemantation
 	-- number of counter's steps to reach teh Dt0
-    constant RANGE_COUNT_COARSE		: positive		:= 2**NUM_OF_SWS -1;
+    
+	
+	constant RANGE_COUNT_SPEED		: positive		:= 2**NUM_OF_SWS -1;
+
+
+	-- RANGE_COUNT_COARSE to RANGE_COUNT_SPEED
 	-- max number that can be set throught the switches
 
 
 	---------------------------- SIGNALS ----------------------------
 
 	----- Counter Registers ----
-	signal	count_fine		:	integer	range	0	to	RANGE_COUNT_FINE-1		:= 0;
-	
+	signal	count_dto		:	integer	range	0	to	RANGE_COUNT_DTO-1		:= 0;
+
+
+	-- count_fine to count_dto
 	-- counter that counts until Dt0
 	
-	signal	count_coarse	:	integer	range	0	to	RANGE_COUNT_COARSE-1	:= 0;
+	signal	count_speed	:	integer	range	0	to	RANGE_COUNT_SPEED-1	:= 0;
 
+	-- count_coarse to count_speed
 	-- counter that counts the Dt0 until the number of Dt0 selected throught the switches
 
-	signal	select_speed	   :	integer	range	0	to	RANGE_COUNT_COARSE-1	:= 1;
-	
+	signal	speed_input	   :	integer	range	0	to	RANGE_COUNT_SPEED-1	:= 1;
+
+	-- select_speed	to speed_input
 	-- sample signal of the value of the unsigned selected throught the switches
 	
-	signal	select_speed_reg   :	integer	range	0	to	RANGE_COUNT_COARSE-1	:= 1;
-	
+	signal	speed_input_reg1   :	integer	range	0	to	RANGE_COUNT_SPEED-1	:= 1;
+	signal	speed_input_reg2   :	integer	range	0	to	RANGE_COUNT_SPEED-1	:= 1;
+
+
+
+	--speed_input_reg to speed_input_reg1
     -- sample signal of the value of the unsigned selected throught the switches, registered for stability
 
 	----------------------------------------------------------------
@@ -76,50 +91,52 @@ begin
 
 		-- rst
 		if rst = '1' then
-			count_fine			<= 0;
-			count_coarse		<= 0;
+			count_dto			<= 0;
+			count_speed		<= 0;
 			count_pulse			<= '0';
 
-			select_speed     	<= 1;
-			select_speed_reg 	<= 1;
-
+			speed_input     	<= 1;
+			speed_input_reg1 	<= 1;
+			speed_input_reg2 	<= 1;
 
 		elsif rising_edge(clk) then
 
-			-- Sample SWs to guarantee stable on rising_edge(clk)
+			-- Sample switches to guarantee stable on rising_edge(clk)
 			--we are thinking about using 3 registered signals, just to show off 
 			-- Because of the bouncing problem
-			select_speed	    <= to_integer(unsigned(SWs));
-			select_speed_reg    <= select_speed;
+			speed_input	    	<= to_integer(unsigned(switches));
+			speed_input_reg1    <= speed_input;
+			speed_input_reg2    <= speed_input_reg1;
+
 
 
 
 
 			-- Count the clock pulses (fine)
-			count_fine	<= count_fine +1;
+			count_dto	<= count_dto +1;
 
-			-- Count the overflow of count_fine (MIN_KITT_CAR_STEP_MS)
+			-- Count the overflow of count_dto (MIN_KITT_CAR_STEP_MS)
 			count_pulse	<= '0';
 
-			if count_fine = RANGE_COUNT_FINE-1 then 
+			if count_dto = RANGE_COUNT_DTO-1 then 
 				--if the minimum steps' counter reaches overflow, the Dt0 counter adds +1
-				count_fine		<= 0;
-				count_coarse	<= count_coarse	+1;
+				count_dto		<= 0;
+				count_speed	<= count_speed	+1;
 
-				-- Count the overflow of count_coarse (MIN_KITT_CAR_STEP_MS*SWs)
-				if count_coarse = select_speed then 
+				-- Count the overflow of count_speed (MIN_KITT_CAR_STEP_MS*switches)
+				if count_speed = speed_input then 
 					--if the #N Dt0 counter reaches overflow, syn goes to 1
-					count_coarse	    <= 0;
+					count_speed	    	<= 0;
 					count_pulse			<= '1';
 				end if;
 
 			end if;
 
 			-- Restart, rst settings
-			if select_speed /= select_speed_reg then 
+			if speed_input_reg1 /= speed_input_reg2 then --Emin değilim ?
 				--go back to lines 141,142: if the two values are different, we have a rst ( I (Laura) have a doubt)
-				count_fine			  <= 0;
-				count_coarse   		  <= 0;
+				count_dto			  <= 0;
+				count_speed   		  <= 0;
 				count_pulse           <= '0';
 			end if;
 
